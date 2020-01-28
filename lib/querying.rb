def48d21b646f2a488f2dd40470df7ce802386d9
Jacqueline Freeland <secretartist272@gmail.com>
2020-01-28 04:01:55 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -17,7 +17,8 @@ def select_value_and_count_of_most_prolific_species
   "SELECT value, count(*)
   FROM characters
   GROUP BY species 
-  "
+  ORDER BY count(species)
+  LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
