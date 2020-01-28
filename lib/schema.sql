49ced46bc933892b9bf50a6c8ddac3619ff192b4
Jacqueline Freeland <secretartist272@gmail.com>
2020-01-28 03:32:29 +0000

    Automatically backed up by Learn

---
+++ lib/schema.sql
@@ -0,0 +1,22 @@
+CREATE TABLE series (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  author TEXT,
+  sub_genre TEXT
+);
+
+CREATE TABLE sub_genres (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE authors (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE books (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  year INTEGER,
+  )
